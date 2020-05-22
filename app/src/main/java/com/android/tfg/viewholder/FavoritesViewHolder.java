b1484c71433cb46759d9fe88ef83f0c974fec8a8
Alejandro <uo243987@uniovi.es>
2020-05-23 01:29:27 +0200

    Item map eliminado (Search & Favorites RecyclerView) Cambiada la vista de favoritos para añadir alertas

--- app/src/main/java/com/android/tfg/viewholder/FavoritesViewHolder.java
+++ app/src/main/java/com/android/tfg/viewholder/FavoritesViewHolder.java
@@ -1,12 +1,16 @@
 package com.android.tfg.viewholder;
 
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.tfg.R;
 import com.android.tfg.databinding.ItemFavoritesBinding;
 import com.android.tfg.model.DeviceModel;
 import com.android.tfg.view.alert.AddAlertDialogFragment;
@@ -36,6 +40,7 @@ public class FavoritesViewHolder extends RecyclerView.ViewHolder {
          * DEVICE INFO *
          ***************/
         binding.itemTitleFav.setText(device.getId());
+        binding.itemTitleFav.setSelected(true);
         binding.itemLocationFav.setText(device.getName());
         binding.itemTempFav.setText(String.valueOf(mainViewModel.convertTemp(device.getLastMessage().getTemp())));
         binding.itemHumFav.setText(String.valueOf(device.getLastMessage().getHum()));
@@ -44,22 +49,11 @@ public class FavoritesViewHolder extends RecyclerView.ViewHolder {
         Date lastUpdated = new Date(device.getLastMessage().getDate().getSeconds()*1000L);
         SimpleDateFormat mFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss", Locale.getDefault());
         binding.itemLastUpdateFav.setText(mFormat.format(lastUpdated));
-
-        /**************
-         * DEVICE MAP *
-         **************/
-        binding.itemMapFav.onCreate(null);
-        binding.itemMapFav.onResume();
-        binding.itemMapFav.onLowMemory();
-        binding.itemMapFav.getMapAsync(googleMap -> {
-            googleMap.getUiSettings().setMapToolbarEnabled(false);
-            googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-            MarkerOptions markerOptions = new MarkerOptions();
-            markerOptions.position(device.getSite());
-            googleMap.addMarker(markerOptions);
-            googleMap.moveCamera(CameraUpdateFactory.newLatLng(device.getSite()));
-            //googleMap.setMinZoomPreference(15);
-        });
+        if(!device.isAlive()){
+            binding.itemCircle.setColorFilter(binding.getRoot().getContext().getResources().getColor(R.color.colorError));
+        }else{
+            binding.itemCircle.setColorFilter(binding.getRoot().getContext().getResources().getColor(R.color.colorAccent));
+        }
 
         /**********************
          * CARD VIEW ON CLICK *
